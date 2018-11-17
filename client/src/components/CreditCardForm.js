d731f6b2f9cb71e3a9fe351357b8ef967a4db3ab
Brian Chan <bchan92@uw.edu>
2018-11-17 10:48:54 -0800

    Refactor CreditCardForm component to use the Form component

--- client/src/components/CreditCardForm.js
+++ client/src/components/CreditCardForm.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Form from './Form';
 
 const initialValues = {
   name: '',
@@ -7,28 +8,10 @@ const initialValues = {
   cvc: '',
 };
 
-class CreditCardForm extends Component {
-  state = initialValues;
-
-  handleChange = event => {
-    event.preventDefault();
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.onSubmit({ ...this.state });
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
+const CreditCardForm = ({ onSubmit }) => (
+  <Form initialValues={initialValues} onSubmit={onSubmit}>
+    {({ values, handleChange, handleSubmit }) => (
+      <form onSubmit={handleSubmit}>
         <div className="field">
           <label className="label">Full Name</label>
           <div className="control">
@@ -37,8 +20,8 @@ class CreditCardForm extends Component {
               placeholder="Taylor Swift"
               type="text"
               name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
+              value={values.name}
+              onChange={handleChange}
               data-testid="name-input"
             />
             <p className="help is-danger">{null}</p>
@@ -52,8 +35,8 @@ class CreditCardForm extends Component {
               placeholder="1234123412341234"
               type="tel"
               name="number"
-              value={this.state.number}
-              onChange={this.handleChange}
+              value={values.number}
+              onChange={handleChange}
               data-testid="number-input"
             />
             <p className="help is-danger">{null}</p>
@@ -69,8 +52,8 @@ class CreditCardForm extends Component {
                   placeholder="MM/YY"
                   type="tel"
                   name="expiry"
-                  value={this.state.expiry}
-                  onChange={this.handleChange}
+                  value={values.expiry}
+                  onChange={handleChange}
                   data-testid="expiry-input"
                 />
                 <p className="help is-danger">{null}</p>
@@ -84,8 +67,8 @@ class CreditCardForm extends Component {
                   placeholder="CVC"
                   type="tel"
                   name="cvc"
-                  value={this.state.cvc}
-                  onChange={this.handleChange}
+                  value={values.cvc}
+                  onChange={handleChange}
                   data-testid="cvc-input"
                 />
                 <p className="help is-danger">{null}</p>
@@ -105,8 +88,8 @@ class CreditCardForm extends Component {
           </div>
         </div>
       </form>
-    );
-  }
-}
+    )}
+  </Form>
+);
 
 export default CreditCardForm;
